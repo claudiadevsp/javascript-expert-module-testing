5d2855f2f58da8ce5eb463fb9b39315b9ab038cd
Claudia Silva <csilva@allin.com.br>
2020-11-10 10:59:30 -0300

    ajusted structure directories

--- aula01-mocks/src/tests/index.test.js
+++ aula01-mocks/src/tests/index.test.js
@@ -1,22 +1,23 @@
-const { error } = require('./src/constants')
-const File = require('./src/file')
+const { error } = require('../constants')
+const File = require('../file')
 const { rejects, deepStrictEqual } = require('assert')
+
 ;
 (async () => {
     {
-        const filePath = './mocks/emptyFile-invalid.csv'
+        const filePath = '../mocks/emptyFile-invalid.csv'
         const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
         const result = File.csvToJson(filePath)
         await rejects(result, rejection)
     }
     {
-        const filePath = './mocks/four-items-invalid.csv'
+        const filePath = '../mocks/four-items-invalid.csv'
         const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE)
         const result = File.csvToJson(filePath)
         await rejects(result, rejection)
     }
     {
-        const filePath = './mocks/threeItems-valid.csv'
+        const filePath = '../mocks/threeItems-valid.csv'
         const result = await File.csvToJson(filePath)
         const expected = [
               {
--- aula02-stubs/src/tests/service.test.js
+++ aula02-stubs/src/tests/service.test.js
@@ -1,11 +1,11 @@
-const Service = require('./service')
+const Service = require('../service')
 const sinon = require('sinon')
 const { deepStrictEqual } = require('assert')
 const BASE_URL_1 = 'https://swapi.dev/api/planets/1/'
 const BASE_URL_2 = 'https://swapi.dev/api/planets/2/'
 const mocks = {
-    tatooine: require('./mocks/tatooine.json'),
-    alderaan: require('./mocks/alderaan.json')
+    tatooine: require('../mocks/tatooine.json'),
+    alderaan: require('../mocks/alderaan.json')
 }
 
 ;(async () => {
